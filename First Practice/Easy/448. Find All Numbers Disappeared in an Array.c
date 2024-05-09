094f2ca0c57e96ea3308ba508983c927c3f71ee2
LucaNoch <139742505+LucaNoch@users.noreply.github.com>
2024-05-10 01:39:51 +0800

    Update 448. Find All Numbers Disappeared in an Array.c

--- First Practice/Easy/448. Find All Numbers Disappeared in an Array.c
+++ First Practice/Easy/448. Find All Numbers Disappeared in an Array.c
@@ -29,3 +29,50 @@ int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
 
     return return_array;
 }
+
+// Solution 2  Less memory
+
+int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+
+    int temp = 0;
+    
+    for (int i = 0; i < numsSize; ++i) {
+        temp = abs(nums[i]) - 1;
+        nums[temp] = abs(nums[temp]) * -1;
+    }
+    
+    int insert_index = 0;
+
+    *returnSize = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        if (nums[i] > 0) {
+            ++*returnSize;
+            nums[insert_index++] = i + 1;
+        }
+    }
+    
+    return nums;
+}
+
+// Solution 3
+
+int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+    int m;
+    for (int c = 0; c < numsSize; c++) {
+        m = abs(nums[c]) - 1;
+        nums[m] = -1 * abs(nums[m]);
+    }
+
+    *returnSize = 0;
+    for (int c = 0; c < numsSize; c++)
+        if (nums[c] > 0)
+            (*returnSize)++;
+    
+    int *ret = (int *) calloc (*returnSize, sizeof (int));
+    int s = 0;
+    for (int c = 0; c < numsSize; c++)
+        if (nums[c] > 0)
+            ret[s++] = c + 1;
+    
+    return ret;
+}
